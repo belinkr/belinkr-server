9a62c2a6976557df5ea7ee29d16ca62705e58e10
Lorenzo Planas <lorenzo.planas@gmail.com>
2012-11-30 13:43:50 +0100

    request models for assign collaborator role and assign administrator role in workspace

---
+++ Cases/AssignAdministratorRoleInWorkspace/Request.rb
@@ -0,0 +1,50 @@
+# encoding: utf-8
+require_relative '../../Resources/User/Member'
+require_relative '../../Resources/Workspace/Member'
+require_relative '../../Resources/Workspace/Enforcer'
+require_relative '../../Services/Tracker'
+
+module Belinkr
+  module AssignAdministratorRoleInWorkspace
+    class Request
+      def initialize(payload, actor, entity)
+        @payload  = payload
+        @actor    = actor
+        @entity   = entity
+      end #initialize
+
+      def prepare
+        {
+          actor:        actor,
+          target_user:  target_user,
+          workspace:    workspace,
+          enforcer:     enforcer,
+          tracker:      tracker
+        }
+      end # prepare
+
+      private
+      
+      attr_accessor :payload, :actor, :entity
+
+      def target_user
+        @target_user ||= User::Member.new(id: payload.fetch('id')).fetch
+      end #target_user
+
+      def workspace
+        @workspace ||= Workspace::Member.new(
+          id:         payload.fetch('workspace_id'),
+          entity_id:  entity.id
+        ).fetch
+      end #workspace
+
+      def enforcer
+        Workspace::Enforcer.new(workspace)
+      end #enforcer
+
+      def tracker
+        Workspace::Tracker.new
+      end #tracker
+    end # Request
+  end # AssignAdministratorRoleInWorkspace
+end # Belinkr
