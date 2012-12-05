7e75768993c5767cf74dd5f9ff7f03a60b62ecb6
David Ruan <ruanwz@gmail.com>
2012-12-05 13:14:08 +0800

    add TireWrapper class

---
+++ Services/Searcher/TireWrapper.rb
@@ -0,0 +1,27 @@
+require 'tire'
+module Belinkr
+  module TireWrapper
+    def index_store(name, hash)
+      Tire.index name do
+        store hash
+        refresh
+      end
+    end
+    def index_create(name)
+      Tire::Index.new(name).create
+    end
+
+    def index_delete(name)
+      Tire::Index.new(name).delete
+    end
+
+    def index_search(name, term)
+      s = Tire.search name do
+        query do
+          string  "name:#{term}"
+        end
+      end
+      s.results
+    end
+  end
+end
